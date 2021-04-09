8a4395d47a3db43da04b4dfd72ba2b87e98a670c
stenhuisen <66888197+stenhuisen@users.noreply.github.com>
2021-04-09 11:25:39 -0400

    fixed function names

--- games/dice_12_sides.py
+++ games/dice_12_sides.py
@@ -1,16 +1,16 @@
 from numpy import random
 
 
-def roll_a_die():
+def roll_a_die_12sides():
     """this function returns a value 1-6"""
     return random.randint(12) + 1
 
 
-def roll_two_dice():
+def roll_two_dice_12sides():
     """this function returns sum of 2 randomly selected values 1-6"""
     return roll_a_die() + roll_a_die()
 
 
-def roll_three_dice():
+def roll_three_dice_12sides():
     """this function returns sum of 3 randomly selected values 1-6"""
     return roll_a_die() + roll_a_die() + roll_a_die()
