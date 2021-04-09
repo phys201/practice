b870262f7ce3b20ae23dd602c21a07864c130ad1
araun890 <82104493+araun890@users.noreply.github.com>
2021-04-09 11:17:09 -0400

    Update coins.py
    Alex Raun has added a hello message in the flip_a_coin function

--- games/coins.py
+++ games/coins.py
@@ -2,5 +2,5 @@ from numpy import random
 
 
 def flip_a_coin():
-    """this function flips a coin"""
+    """this function flips a coin, hello from alex raun"""
     return random.choice(['heads', 'tails'])
