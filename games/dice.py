75c364e0ca39d12392d97bd6fb4f0d3ced80f69a
tcontrer <taylorcontreras@g.harvard.edu>
2021-04-09 11:13:35 -0400

    Fix d20 bug, add roll_two_d20, add roll_three_d20

--- games/dice.py
+++ games/dice.py
@@ -17,4 +17,13 @@ def roll_three_dice():
 
 def roll_d20():
     """this function returns a value 1-20"""
-    return random.randint(6) + 1
+    return random.randint(20) + 1
+
+def roll_two_d20():
+    """this function returns sum of 2 randomly selected values 1-20"""
+    return roll_a_die() + roll_a_die()
+
+
+def roll_three_d20():
+    """this function returns sum of 3 randomly selected values 1-20"""
+    return roll_d20() + roll_d20() + roll_d20()
