9ec4a476f20c5f0b46417e1079c6c738581d17dd
spacemira <44369231+spacemira@users.noreply.github.com>
2021-04-09 11:12:29 -0400

    Add a d20 to dice.py
    Adding a function to roll a d20 in dice.py

--- games/dice.py
+++ games/dice.py
@@ -14,3 +14,7 @@ def roll_two_dice():
 def roll_three_dice():
     """this function returns sum of 3 randomly selected values 1-6"""
     return roll_a_die() + roll_a_die() + roll_a_die()
+
+def roll_a_d20():
+    """this function returns a value 1-20"""
+    return random.randint(20)+1
