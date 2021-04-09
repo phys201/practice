fd4e610c6656333dbacba319dfaa6e72e1ec74c5
mcooper12 <55110657+mcooper12@users.noreply.github.com>
2021-04-09 11:32:08 -0400

    add d20(n)
    Added d20(n) function to dice

--- games/__init__.py
+++ games/__init__.py
@@ -1,3 +1,3 @@
 from games.cards import pick_a_card
 from games.coins import flip_a_coin
-from games.dice import roll_a_die, roll_two_dice, roll_three_dice
+from games.dice import roll_a_die, roll_two_dice, roll_three_dice, d20
--- games/dice.py
+++ games/dice.py
@@ -1,4 +1,5 @@
 from numpy import random
+import numpy as np
 
 
 def roll_a_die():
@@ -14,3 +15,8 @@ def roll_two_dice():
 def roll_three_dice():
     """this function returns sum of 3 randomly selected values 1-6"""
     return roll_a_die() + roll_a_die() + roll_a_die()
+
+def d20(n):
+    '''this function rolls n 20-sided dice and reports the results'''
+    rolls = [random.randint(20)+1 for i in range(n)] 
+    return rolls
