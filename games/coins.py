7090588e8cc8fbf615c4cb42fd823ba24699feec
Eliot F. Fenton <eliotfenton@gmail.com>
2022-04-08 11:19:32 -0400

    Update coins.py

--- games/coins.py
+++ games/coins.py
@@ -3,5 +3,5 @@ from numpy import random
 
 def flip_a_coin():
     """this function flips a coin"""
-    print("Come on tails!")
-    return random.choice(['heads', 'tails'])
+    print("Tails never fails!")
+    return random.choice(["heads", "tails"])
