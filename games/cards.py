71da77dc8eca13c419d09750e6683eed8a37ebb8
aspitzig0 <60667685+aspitzig0@users.noreply.github.com>
2022-04-08 11:30:47 -0400

    Update cards.py

--- games/cards.py
+++ games/cards.py
@@ -4,6 +4,12 @@ from numpy import random
 def pick_a_card():
     """this function picks a card """
     value = random.randint(1, 14)
+    if value == 11:
+        value = 'J'
+    else if value == 12:
+        value = 'Q'
+    else if value == 13:
+        value = 'K'
     suit = random.randint(4)
     suit = ['clubs', 'diamonds', 'hearts', 'spades'][suit]
     return (value, suit)
