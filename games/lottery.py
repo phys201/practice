5cf9ed0dd0e3a3c9cdd9e7c10f6b716083372ea8
kelliannekornick <kellianne.kornick@gmail.com>
2023-04-07 11:33:24 -0400

    Update lottery.py

--- games/lottery.py
+++ games/lottery.py
@@ -2,6 +2,7 @@ from numpy import random
 
 
 def generate_ticket():
-    """this function generates a lottery ticket with 8 numbers"""
+    """this function generates a lottery ticket with 8 numbers,
+    each from 1 to 100"""
     values = random.randint(1, 100, 8)
     return list(values)
