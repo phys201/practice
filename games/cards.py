0e83bae96dbbf4bc96c501e37ad2ebf053f12be0
aklunstad <aklunstad@gmail.com>
2023-04-07 11:26:55 -0400

    Update cards.py
    Updated the name for clubs to practice committing in Github workflow

--- games/cards.py
+++ games/cards.py
@@ -4,5 +4,5 @@ def pick_a_card():
     """this function picks a card """
     value = random.randint(1, 14)
     suit = random.randint(4)
-    suit = ['clubs', 'diamonds', 'hearts', 'spades'][suit]
+    suit = ['Annikas suit test', 'diamonds', 'hearts', 'spades'][suit]
     return (value, suit)
