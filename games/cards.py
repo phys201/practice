eca3accc9cceee40b3f798243121cb02eab21512
pablotting <yiting_huang@college.harvard.edu>
2023-04-07 11:28:34 -0400

    Update cards.py
    Updated a print line in pick_a_card()

--- games/cards.py
+++ games/cards.py
@@ -5,4 +5,5 @@ def pick_a_card():
     value = random.randint(1, 14)
     suit = random.randint(4)
     suit = ['clubs', 'diamonds', 'hearts', 'spades'][suit]
+    print('the suit is ', suit)
     return (value, suit)
