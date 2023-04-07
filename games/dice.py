0d7e5442e5974c73882644b4be03f34919a994b4
alexcui1 <42044923+alexcui1@users.noreply.github.com>
2023-04-07 11:23:01 -0400

    Update dice.py
    Updated dice to include more explanation for the code

--- games/dice.py
+++ games/dice.py
@@ -2,5 +2,5 @@ from numpy import random, sum
 
 
 def roll_a_die(n):
-    """this function returns a value 1-n"""
+    """this function returns a value 1-n, add 1 so the number start from 1"""
     return random.randint(n) + 1
