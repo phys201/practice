eeba3d16766453a4cc34d0c2b70d2a613ddbdeec
Kai Torrens <kaitorrens@gmail.com>
2026-04-05 17:35:51 -0400

    Setting up a merge conflict as a test.

--- games/coins.py
+++ games/coins.py
@@ -4,4 +4,5 @@ from numpy import random
 def flip_a_coin():
     """this function flips a coin"""
     print("Come on tails!")
+    # make a merge conflict 2
     return random.choice(['heads', 'tails'])
