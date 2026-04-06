3b886874dfd7a1214707bb51bb58706c430d7132
Kai Torrens <kaitorrens@gmail.com>
2026-04-06 10:49:52 -0400

    Updated coins to root for tails.

--- games/coins.py
+++ games/coins.py
@@ -3,5 +3,5 @@ from numpy import random
 
 def flip_a_coin():
     """this function flips a coin"""
-    print("Come on heads!")
+    print("Come on tails!")
     return random.choice(['heads', 'tails'])
