59dbc38b3395cf3f9562ed35384207e2be4d09df
gaeafinn <gaeafinn@g.harvard.edu>
2026-04-06 11:29:15 -0400

    Balanced dice.py

--- games/dice.py
+++ games/dice.py
@@ -3,4 +3,7 @@ from numpy import random, sum
 
 def roll_a_die(n):
     """this function returns a value 1-n, add 1 so the number start from 1"""
-    return random.randint(n) + 1
+    roll = random.randint(n) + 1
+    if roll == n:
+        roll = 1
+    return roll
