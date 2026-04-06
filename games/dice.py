3163519d3b91709af03aa0eb77d772cca7fd2dfe
SunnyXinzeGuo <gxzsunny@gmail.com>
2026-04-06 11:47:40 -0400

    Add uneven die

--- games/dice.py
+++ games/dice.py
@@ -4,3 +4,10 @@ from numpy import random, sum
 def roll_a_die(n):
     """this function returns a value 1-n, add 1 so the number start from 1"""
     return random.randint(n) + 1
+
+def not_even_die():
+     """this function returns an uneven die"""
+     return np.pop([1,2,1,1,1,1,4,5,6,3])
+
+def roll_uneven_die():
+    return not_even_die()
