c17cc9ea3374624a87498aac11dd03c2ce6f36cc
Kai Torrens <kaitorrens@gmail.com>
2026-04-06 11:07:53 -0400

    Example merge conflict.

--- games/coins.py
+++ games/coins.py
@@ -4,4 +4,5 @@ from numpy import random
 def flip_a_coin():
     """this function flips a coin"""
     print("Come on tails!")
+    # purposeful merge conflict 1
     return random.choice(['heads', 'tails'])
